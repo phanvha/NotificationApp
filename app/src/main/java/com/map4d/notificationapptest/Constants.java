c06290010d07676fdebbb19b8573ae36d2135eb2
phanvha <pvha.17it3@sict.udn.vn>
2019-10-14 15:19:45 +0700

    Kiểm tra  gửi và lưu token

---
+++ app/src/main/java/com/map4d/notificationapptest/Constants.java
@@ -0,0 +1,8 @@
+package com.map4d.notificationapptest;
+
+public class Constants {
+    public static final String URL_REGISTER_DEVICE = "https://vbusapp.000webhostapp.com/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "https://vbusapp.000webhostapp.com/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "https://vbusapp.000webhostapp.com/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "https://vbusapp.000webhostapp.com/GetRegisteredDevices.php";
+}
