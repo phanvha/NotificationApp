e1dbf4739e134599396cec7b201e572977e5b49c
phanvha <pvha.17it3@sict.udn.vn>
2019-10-18 08:23:16 +0700

    Notify thành công. đang hoàn thiện chức năng và giao diện

--- app/src/main/java/com/map4d/notificationapptest/MyFirebaseMessagingService.java
+++ app/src/main/java/com/map4d/notificationapptest/MyFirebaseMessagingService.java
@@ -1,15 +1,21 @@
 package com.map4d.notificationapptest;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Looper;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -23,8 +29,15 @@ import com.google.firebase.messaging.RemoteMessage;
 import com.map4d.model.Data;
 import com.map4d.service.APIClient;
 import com.map4d.utils.APIInterface;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.Executor;
 
 import okhttp3.FormBody;
@@ -36,17 +49,57 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "MyFirebaseService";
+
+    private static final String TAG = "MyFirebaseMsgService";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .build();
-        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-        manager.notify(123, notification);
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                sendPushNotification(json);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception: " + e.getMessage());
+            }
+        }
     }
+
+    //this method will display the notification
+    //We are passing the JSONObject that is received from
+    //firebase cloud messaging
+    private void sendPushNotification(JSONObject json) {
+        //optionally we can display the json into log
+        Log.e(TAG, "Notification JSON " + json.toString());
+        try {
+            //getting the json data
+            JSONObject data = json.getJSONObject("data");
+
+            //parsing json data
+            String title = data.getString("title");
+            String message = data.getString("message");
+            String imageUrl = data.getString("image");
+
+            //creating MyNotificationManager object
+            MyNotificationManager mNotificationManager = new MyNotificationManager(getApplicationContext());
+
+            //creating an intent for the notification
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+            //if there is no image
+            if(imageUrl.equals("null")){
+                //displaying small notification
+                mNotificationManager.showSmallNotification(title, message, intent);
+            }else{
+                //if there is an image
+                //displaying a big notification
+                mNotificationManager.showBigNotification(title, message, imageUrl, intent);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
 }
